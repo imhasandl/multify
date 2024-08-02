2425b05cab9d42096e33dfb9d6a9a9bcbce68d1e
imhasandl <imhasandl04@gmail.com>
2024-08-03 02:42:27 +0500

    the rest theme changing components

--- app/components/LeftNav.tsx
+++ app/components/LeftNav.tsx
@@ -17,9 +17,9 @@ export default function SideNav() {
 
   return (
     <nav 
-      className={`h-[100vh] w-full min-w-[250px] lg:min-w-[300px] lg:max-w-[350px] z-50 border-[#4A596D] border-8 hidden lg:block sticky top-0 rounded-r-lg hover:shadow-2xl transition duration-150
+      className={`h-[100vh] w-full min-w-[250px] lg:min-w-[300px] lg:max-w-[350px] z-50 hidden lg:block sticky top-0 shadow-2xl transition duration-150
       ${theme ? 'bg-[#282828] text-white' : 'bg-white text-black'}  
-    `}>
+    `} > 
       {/* Logo Section // CHANGE THE LOGO AS SOON AS POSIBLe */}
       <div className="flex flex-col items-center w-full h-[200px] p-2">
         <Link href='/'>
@@ -40,12 +40,7 @@ export default function SideNav() {
         <div className={`absolute top-[170px] h-[2px] w-[200px] rounded-lg ${theme ? 'bg-white' : 'bg-[#4A596D]'}`} />
       </div>
 
-      {/* Account Section Not Working for now */}
-      <div className="flex justify-center  w-full h-[225px] border-black border-4">
-        Profile Stuff
-
-        <div className={`absolute top-[447px] h-[2px] w-[200px] rounded-lg ${theme ? 'bg-white' : 'bg-[#4A596D]'}`} />
-      </div>
+      
 
       {/* Theme Button Section  */}
       <div className="absolute h-[50px] w-[50px] border-4 right-0 bottom-0">
--- app/components/ThemeButton.tsx
+++ app/components/ThemeButton.tsx
@@ -19,8 +19,6 @@ export default function ThemeButton() {
     const turnLightTheme = () => {
       dispatch(turnLightMode())
   }
-    
-
 
   return (
     <>
