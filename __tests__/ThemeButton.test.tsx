07effb123639ace5d13580db21f0a27fe211c347
imhasandl <imhasandl04@gmail.com>
2024-08-03 01:16:49 +0500

    jest-testing-library

---
+++ __tests__/ThemeButton.test.tsx
@@ -0,0 +1,16 @@
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import ThemeButton from '@/app/components/ThemeButton';
+import { Provider } from 'react-redux';
+import store from '@/app/features/store';
+
+describe('ThemeButton', () => {
+  test('renders the component', () => {
+    render(
+      <Provider store={store}>
+        <ThemeButton />
+      </Provider>
+    );
+    expect(screen.getByText('fd')).toBeInTheDocument();
+  });
+});
---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transformIgnorePatterns: ['<rootDir>/node_modules/'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+  },
+};
