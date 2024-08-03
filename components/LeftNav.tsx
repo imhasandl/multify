4393ef971bb1a72e9bd91b2cf65ad4e9c88b913c
imhasandl <imhasandl04@gmail.com>
2024-08-04 01:18:12 +0500

    buttons-design+functinality

--- components/LeftNav.tsx
+++ components/LeftNav.tsx
@@ -28,7 +28,7 @@ export default function SideNav() {
   return (
     <nav 
       className={`h-[100vh] w-full min-w-[250px] lg:min-w-[300px] lg:max-w-[350px] z-50 hidden lg:block sticky top-0 shadow-2xl transition duration-150
-      ${theme ? 'bg-[#282828] text-white' : 'bg-white text-black'}  
+      ${theme ? 'bg-[#282828] text-white' : 'bg-white'}  
     `} > 
       {/* Logo Section // CHANGE THE LOGO AS SOON AS POSIBLe */}
       <div className="flex flex-col items-center w-full h-[200px] p-2">
@@ -53,11 +53,14 @@ export default function SideNav() {
       <div className="flex flex-col m-3 gap-2">
         {navButtonsOptons.map(option => (
           <Link href={option.route}>
-            <div className="flex justify-center items-center border-4 rounded-md hover:shadow-md transition-all">
-              <button key={option.id} className="flex w-full py-2">{option.name}</button>
-              <Image className="w-[25px] h-[25px]" src={arrowRight} alt="arrowRight" />
-            </div>
-          </Link>
+          <div className={`flex justify-center items-center rounded-md hover:shadow-md transition-all transform hover:scale-110
+            ${theme ? 'bg-gradient-to-r from-[#282828] to-[#4A596D]' : 'bg-gradient-to-r from-white to-[#4A596D]'}`}>
+            <button key={option.id} className="flex w-full py-2">
+              <span className="font-sans ml-5">{option.name}</span>
+            </button>
+            <Image className="w-[25px] h-[25px]" src={arrowRight} alt="arrowRight" />
+          </div>
+        </Link>
         ))}
       </div>
 
