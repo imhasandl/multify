fd6b703f4a5fa2f3b3325c79ee26ca9fe50418a2
imhasandl <imhasandl04@gmail.com>
2024-08-03 23:37:28 +0500

    test

--- app/components/TopNav.tsx
+++ app/components/TopNav.tsx
@@ -11,7 +11,7 @@ export default function TopNav() {
 
   return (
     <nav 
-      className={`flex justify-center items-center lg:hidden w-full h-[100px] sticky top-0 z-50 duration-150 shadow-lg rounded-b-lg
+      className={`flex justify-center items-center lg:hidden w-full h-[100px] sticky top-0 z-50 duration-150 shadow-lg
          ${theme ? "bg-[#282828] text-white" : 'bg-white'}
       `}>
       <div className="absolute right-2 w-[55px] h-[55px]">
