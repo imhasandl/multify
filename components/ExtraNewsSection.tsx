1ad31987ef2f81760a1d09de3424a3ab4a81ea0e
imhasandl <imhasandl04@gmail.com>
2024-08-06 05:02:02 +0500

    search functinality

--- components/ExtraNewsSection.tsx
+++ components/ExtraNewsSection.tsx
@@ -5,88 +5,104 @@ import { useState, useEffect } from "react"
 import search from '@/public/search.svg'
 import categories from "@/data/categoryData"
 import countries from "@/data/countriesData"
+import preLoaderSVG from '@/public/bouncing-circles.svg'
+import Image from "next/image"
 
 export default function extraNewsSection({ themeMode }: any) {
   const [news, setNews] = useState([])
+  const [selectedCategory, setSelectedCategory] = useState<string>('')
+  const [selectedCountry, setSelectedCountry] = useState<string>('')
+  const [result, setResult] = useState<any>()
+  const [loading, setLoading] = useState<boolean>(false) 
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(`https://newsapi.org/v2/everything?q=technology&apiKey=38546be38b424463a794093a83b5822c&pageSize=15`)
-        if(!response.ok){
-          throw new Error("Something went wrong")
-        }
-        const data = await response.json()
-          // FILTERS
-        const filteredNews = data.articles
-          .filter((article: any) => article.urlToImage !== null)
-          .filter((article: any) => article.author !== null)
-          .filter((article: any) => article.description !== null)
-          .filter((article: any) => article.title !== null)
-          .filter((article: any) => article.content !== null)
-          .filter((article: any) => article.publishedAt !== null);
-         
-        setNews(filteredNews)
-      } catch (error) {
-        console.log(error)
+  async function handleSearch(){
+    try {
+      setLoading(true)
+      const responce = await fetch(`
+        https://newsapi.org/v2/top-headlines?country=${selectedCountry}&category=${selectedCategory}&apiKey=38546be38b424463a794093a83b5822c
+      `)
+      if(!responce.ok){
+        throw new Error("Something went wrong")
       }
+      const data = await responce.json()
+      // const slicedData = data.articles.slice(0, 10)
+
+      setResult(data.articles)
+      setLoading(false)
+    } catch (error) {
+      alert(error)
     }
+  }
 
-    fetchData()
-  }, [])
-
-  console.log(themeMode)
+  console.log(result)
 
   return (
     <div className="flex flex-col relative flex-wrap m-2 md:mx-8">
-      {/* Input Section */}
-      <div className="flex flex-row justify-start">
-        <div>
-          <h1 className="font-semibold font-sans">Search for a specific thing: </h1>
-          <div className="flex flex-row justify-start hover:opacity-80 transition duration-300">
-            <input className={`${themeMode ? 'bg-[#282828]' : "bg-[#F5F5F5]"} px-3 py-[5px] min-w-[300px] border-[#707a8a] border-4 rounded-md`} type="text"/>
-          </div>
-        </div>
-
+      <div className="flex flex-row flex-wrap justify-between items-center">
         {/* Filter Section */}
-        <div className="flex flex-col ml-4 font-semibold font-san">
-          <h1>Category: </h1>
-          <div className=" border-[#707a8a] border-4 rounded-md">
-            <select className={`${themeMode ? 'bg-[#282828]' : "bg-[#F5F5F5]"} font-bold text-md px-3 py-[6px] rounded-md`}>
-              <option value="">Select:</option>
-              {categories.map((data, index) => (
-                <option key={index} value={data.value}>
-                  {data.category}
-                </option>
-              ))}
-            </select>
+        <div className="flex flex-row ">
+          <div className="flex flex-col font-semibold font-sans">
+            <h1>Category: </h1>
+            <div className=" border-[#707a8a] border-4 rounded-md">
+              <select 
+                className={`${themeMode ? 'bg-[#282828]' : "bg-[#F5F5F5]"} font-bold text-md px-3 py-[6px] rounded-md`}
+                onChange={(e) => setSelectedCategory(e.target.value)}
+              >
+                <option value="">Select:</option>
+                {categories.map((data, index) => (
+                  <option key={index} value={data.value}>
+                    {data.category}
+                  </option>
+                ))}
+              </select>
+            </div>
+          </div>
+
+          <div className="flex flex-col font-semibold font-sans ml-8">
+            <h1>Country: </h1>
+            <div className=" border-[#707a8a] border-4 rounded-md">
+              <select 
+                className={`${themeMode ? 'bg-[#282828]' : "bg-[#F5F5F5]"} font-bold text-md px-3 py-[6px] rounded-md`}
+                onChange={(e) => setSelectedCountry(e.target.value)}
+              >
+                <option value="">Select:</option>
+                {countries.map(data => (
+                  <option key={data.country} value={data.isoCode}>
+                    {data.country}
+                  </option>
+                ))}
+              </select>
+            </div>
           </div>
         </div>
 
-        <div className="flex flex-col ml-4 font-semibold font-sans">
-          <h1>Country: </h1>
-          <div className=" border-[#707a8a] border-4 rounded-md">
-            <select className={`${themeMode ? 'bg-[#282828]' : "bg-[#F5F5F5]"} font-bold text-md px-3 py-[6px] rounded-md`}>
-              <option value="">Select:</option>
-              {countries.map(data => (
-                <option key={data.country} value={data.isoCode}>
-                  {data.country}
-                </option>
-              ))}
-            </select>
-          </div>
-        </div>
-
-        <div className="absolute right-5 ">
-          <button className="p-4 h-auto">Search</button>
+        <div>
+          <button onClick={handleSearch} className={`${themeMode ? 'bg-[#121212]' : 'bg-white'} border-gray-600 border-4 w-[150px] h-[50px] rounded-md hover:opacity-75`}>
+            Search
+          </button>
         </div>
       </div>
 
 
       {/* Result News */}
-      <div className="h-[500px] mt-8">
-        Result
+      <div className="flex justify-center mt-8">
+        {loading ? (
+          <Image 
+            src={preLoaderSVG}
+            alt="Loader"
+            className="mt-[150px] min-w-[185px]"
+          />
+        ) : result ? (
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            {result.map((article: any, index: number) => (
+              <div key={index} className="flex flex-row">
+                {article.title}
+              </div>
+            ))}
+          </div>
+        ) : null}
       </div>
+
     </div>
   )
 }
