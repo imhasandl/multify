c1046149bdb385a0ea05d38eed386be169aad732
imhasandl <imhasandl04@gmail.com>
2024-08-08 01:10:41 +0500

    css changes

--- app/todos/page.tsx
+++ app/todos/page.tsx
@@ -9,18 +9,20 @@ export default function page() {
   return (
     <div className={`${theme ? 'bg-[#121212]' : 'bg-[#F5F5F5]'} w-full flex justify-center transition duration-150`}>
       <div className={`${theme ? 'bg-[#282828] text-white' : 'bg-white'} max-w-[1150px] w-full transition duration-150 shadow-2xl z-1`}>
-        {/* Todo / Diary / Calender */}
         <div className="w-full flex flex-col  h-[100vh]">
-          <div className="flex flex-row justify-evenly mx-4 my-6">
-            <button className="text-2xl p-3 w-[175px] rounded-lg hover:bg-gray-300 transition hover:text-black">Todo</button>
-            <button className="text-2xl p-3 w-[175px] rounded-lg hover:bg-gray-300 transition hover:text-black">Diary</button>
-            <button className="text-2xl p-3 w-[175px] rounded-lg hover:bg-gray-300 transition hover:text-black">Calendar</button>
+          {/* Todo / Diary / Calender */}
+          <div className="flex flex-col">
+            <div className="flex flex-row justify-evenly mx-4 my-6">
+              <button className="text-2xl p-3 w-[175px] rounded-lg hover:bg-gray-300 transition hover:text-black">Todo</button>
+              <button className="text-2xl p-3 w-[175px] rounded-lg hover:bg-gray-300 transition hover:text-black">Diary</button>
+              <button className="text-2xl p-3 w-[175px] rounded-lg hover:bg-gray-300 transition hover:text-black">Calendar</button>
+            </div>
+
+            <div className="h-[4px] mx-8 bg-black bg-gradient-to-r from-cyan-500 to-blue-500 rounded-lg" />
           </div>
 
-          <div className="h-[4px] mx-8 bg-black bg-gradient-to-r from-cyan-500 to-blue-500 rounded-lg" />
+
         </div>
-
-
       </div>
     </div>
   )
